ab284f0ceea3330c24727b5af0098e188593d8e6
andrewpowell <andrew.powell@universalmind.com>
2014-07-23 16:53:18 -0400

    adding the ability to render multiple pages

--- ng-pdfviewer.js
+++ ng-pdfviewer.js
@@ -1,154 +1,171 @@
 /**
  * @preserve AngularJS PDF viewer directive using pdf.js.
  *
- * https://github.com/akrennmair/ng-pdfviewer 
+ * https://github.com/akrennmair/ng-pdfviewer
  *
  * MIT license
  */
 
 angular.module('ngPDFViewer', []).
-directive('pdfviewer', [ '$parse', function($parse) {
-	var canvas = null;
-	var instance_id = null;
+    directive('pdfviewer', [ '$parse', function($parse) {
+        var canvas = null;
+        var instance_id = null;
 
-	return {
-		restrict: "E",
-		template: '<canvas></canvas>',
-		scope: {
-			onPageLoad: '&',
-			loadProgress: '&',
-			src: '@',
-			id: '='
-		},
-		controller: [ '$scope', function($scope) {
-			$scope.pageNum = 1;
-			$scope.pdfDoc = null;
-			$scope.scale = 1.0;
+        return {
+            restrict: "E",
+            template: '<canvas></canvas>',
+            scope: {
+                onPageLoad: '&',
+                loadProgress: '&',
+                displayAll: '@',
+                src: '@',
+                id: '='
+            },
+            controller: [ '$scope', function($scope) {
+                $scope.pageNum = 1;
+                $scope.pdfDoc = null;
+                $scope.scale = 1.0;
 
-			$scope.documentProgress = function(progressData) {
-				if ($scope.loadProgress) {
-					$scope.loadProgress({state: "loading", loaded: progressData.loaded, total: progressData.total});
-				}
-			};
+                $scope.documentProgress = function(progressData) {
+                    if ($scope.loadProgress) {
+                        $scope.loadProgress({state: "loading", loaded: progressData.loaded, total: progressData.total});
+                    }
+                };
 
-			$scope.loadPDF = function(path) {
-				console.log('loadPDF ', path);
-				PDFJS.getDocument(path, null, null, $scope.documentProgress).then(function(_pdfDoc) {
-					$scope.pdfDoc = _pdfDoc;
-					$scope.renderPage($scope.pageNum, function(success) {
-						if ($scope.loadProgress) {
-							$scope.loadProgress({state: "finished", loaded: 0, total: 0});
-						}
-					});
-				}, function(message, exception) {
-					console.log("PDF load error: " + message);
-					if ($scope.loadProgress) {
-						$scope.loadProgress({state: "error", loaded: 0, total: 0});
-					}
-				});
-			};
+                $scope.loadPDF = function(path) {
+                    console.log('loadPDF ', path);
+                    PDFJS.getDocument(path, null, null, $scope.documentProgress).then(function(_pdfDoc) {
+                        $scope.pdfDoc = _pdfDoc;
+                        $scope.numPages = $scope.pdfDoc.numPages;
+                        if($scope.displayAll == "true"){
+                            $scope.pdfDoc.getPage($scope.pageNum).then($scope.renderAll)
+                        }else{
+                            $scope.renderPage($scope.pageNum, function(success) {
+                                if ($scope.loadProgress) {
+                                    $scope.loadProgress({state: "finished", loaded: 0, total: 0});
+                                }
+                            });
+                        }
+                    }, function(message, exception) {
+                        console.log("PDF load error: " + message);
+                        if ($scope.loadProgress) {
+                            $scope.loadProgress({state: "error", loaded: 0, total: 0});
+                        }
+                    });
+                };
 
-			$scope.renderPage = function(num, callback) {
-				console.log('renderPage ', num);
-				$scope.pdfDoc.getPage(num).then(function(page) {
-					var viewport = page.getViewport($scope.scale);
-					var ctx = canvas.getContext('2d');
+                $scope.renderAll=function(page){
+                    $scope.renderPage($scope.pageNum);
+                    //Move to next page
+                    $scope.pageNum++;
+                    if ( $scope.pdfDoc !== null && $scope.pageNum <= $scope.numPages )
+                    {
+                        $scope.pdfDoc.getPage( $scope.pageNum ).then( $scope.renderAll );
+                    }
+                };
 
-					canvas.height = viewport.height;
-					canvas.width = viewport.width;
+                $scope.renderPage = function(num, callback) {
+                    console.log('renderPage ', num);
+                    $scope.pdfDoc.getPage(num).then(function(page) {
+                        var viewport = page.getViewport($scope.scale);
 
-					page.render({ canvasContext: ctx, viewport: viewport }).promise.then(
-						function() { 
-							if (callback) {
-								callback(true);
-							}
-							$scope.$apply(function() {
-								$scope.onPageLoad({ page: $scope.pageNum, total: $scope.pdfDoc.numPages });
-							});
-						}, 
-						function() {
-							if (callback) {
-								callback(false);
-							}
-							console.log('page.render failed');
-						}
-					);
-				});
-			};
+                        var ctx = canvas.getContext('2d');
 
-			$scope.$on('pdfviewer.nextPage', function(evt, id) {
-				if (id !== instance_id) {
-					return;
-				}
+                        canvas.height = viewport.height;
+                        canvas.width = viewport.width;
 
-				if ($scope.pageNum < $scope.pdfDoc.numPages) {
-					$scope.pageNum++;
-					$scope.renderPage($scope.pageNum);
-				}
-			});
+                        page.render({ canvasContext: ctx, viewport: viewport }).then(
+                            function() {
+                                if (callback) {
+                                    callback(true);
+                                }
+                                $scope.$apply(function() {
+                                    $scope.onPageLoad({ page: $scope.pageNum, total: $scope.pdfDoc.numPages });
+                                });
+                            },
+                            function() {
+                                if (callback) {
+                                    callback(false);
+                                }
+                                console.log('page.render failed');
+                            }
+                        );
+                    });
+                };
 
-			$scope.$on('pdfviewer.prevPage', function(evt, id) {
-				if (id !== instance_id) {
-					return;
-				}
+                $scope.$on('pdfviewer.nextPage', function(evt, id) {
+                    if (id !== instance_id) {
+                        return;
+                    }
 
-				if ($scope.pageNum > 1) {
-					$scope.pageNum--;
-					$scope.renderPage($scope.pageNum);
-				}
-			});
+                    if ($scope.pageNum < $scope.pdfDoc.numPages) {
+                        $scope.pageNum++;
+                        $scope.renderPage($scope.pageNum);
+                    }
+                });
 
-			$scope.$on('pdfviewer.gotoPage', function(evt, id, page) {
-				if (id !== instance_id) {
-					return;
-				}
+                $scope.$on('pdfviewer.prevPage', function(evt, id) {
+                    if (id !== instance_id) {
+                        return;
+                    }
 
-				if (page >= 1 && page <= $scope.pdfDoc.numPages) {
-					$scope.pageNum = page;
-					$scope.renderPage($scope.pageNum);
-				}
-			});
-		} ],
-		link: function(scope, iElement, iAttr) {
-			canvas = iElement.find('canvas')[0];
-			instance_id = iAttr.id;
+                    if ($scope.pageNum > 1) {
+                        $scope.pageNum--;
+                        $scope.renderPage($scope.pageNum);
+                    }
+                });
 
-			iAttr.$observe('src', function(v) {
-				console.log('src attribute changed, new value is', v);
-				if (v !== undefined && v !== null && v !== '') {
-					scope.pageNum = 1;
-					scope.loadPDF(scope.src);
-				}
-			});
-		}
-	};
-}]).
-service("PDFViewerService", [ '$rootScope', function($rootScope) {
+                $scope.$on('pdfviewer.gotoPage', function(evt, id, page) {
+                    if (id !== instance_id) {
+                        return;
+                    }
 
-	var svc = { };
-	svc.nextPage = function() {
-		$rootScope.$broadcast('pdfviewer.nextPage');
-	};
+                    if (page >= 1 && page <= $scope.pdfDoc.numPages) {
+                        $scope.pageNum = page;
+                        $scope.renderPage($scope.pageNum);
+                    }
+                });
+            } ],
+            link: function(scope, iElement, iAttr) {
+                div = iElement.find('canvas')[0];
+                instance_id = iAttr.id;
 
-	svc.prevPage = function() {
-		$rootScope.$broadcast('pdfviewer.prevPage');
-	};
+                iAttr.$observe('src', function(v) {
+                    console.log('src attribute changed, new value is', v);
+                    if (v !== undefined && v !== null && v !== '') {
+                        scope.pageNum = 1;
+                        scope.loadPDF(scope.src);
+                    }
+                });
+            }
+        };
+    }]).
+    service("PDFViewerService", [ '$rootScope', function($rootScope) {
 
-	svc.Instance = function(id) {
-		var instance_id = id;
+        var svc = { };
+        svc.nextPage = function() {
+            $rootScope.$broadcast('pdfviewer.nextPage');
+        };
 
-		return {
-			prevPage: function() {
-				$rootScope.$broadcast('pdfviewer.prevPage', instance_id);
-			},
-			nextPage: function() {
-				$rootScope.$broadcast('pdfviewer.nextPage', instance_id);
-			},
-			gotoPage: function(page) {
-				$rootScope.$broadcast('pdfviewer.gotoPage', instance_id, page);
-			}
-		};
-	};
+        svc.prevPage = function() {
+            $rootScope.$broadcast('pdfviewer.prevPage');
+        };
 
-	return svc;
-}]);
+        svc.Instance = function(id) {
+            var instance_id = id;
+
+            return {
+                prevPage: function() {
+                    $rootScope.$broadcast('pdfviewer.prevPage', instance_id);
+                },
+                nextPage: function() {
+                    $rootScope.$broadcast('pdfviewer.nextPage', instance_id);
+                },
+                gotoPage: function(page) {
+                    $rootScope.$broadcast('pdfviewer.gotoPage', instance_id, page);
+                }
+            };
+        };
+
+        return svc;
+    }]);
